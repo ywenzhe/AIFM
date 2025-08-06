96ef93a74de8a26f198924db8bd5ff8ec98bdbf7
ywenzhe <ywenzhe@outlook.com>
2025-08-06 01:50:08 -0600

    add comment to aifm/exp/fig6a/linux_mem/main.cpp

--- aifm/exp/fig6a/linux_mem/main.cpp
+++ aifm/exp/fig6a/linux_mem/main.cpp
@@ -62,7 +62,7 @@ private:
   constexpr static uint32_t kNumKeysPerRequest = 32;
   constexpr static uint32_t kNumReqs = kNumKVPairs / kNumKeysPerRequest;
   constexpr static uint32_t kLog10NumKeysPerRequest =
-      helpers::static_log(10, kNumKeysPerRequest);
+      helpers::static_log(10, kNumKeysPerRequest); // 对 kNumKeysPerRequest 静态取 log 值
   constexpr static uint32_t kReqLen = kKeyLen - kLog10NumKeysPerRequest;
   constexpr static uint32_t kReqSeqLen = kNumReqs;
 
@@ -89,13 +89,14 @@ private:
     uint8_t data[kArrayEntrySize];
   };
 
+  // alignas(64) 确保声明的变量或类型将以 64 字节进行内存对齐
   struct alignas(64) Cnt {
     uint64_t c;
-  };
+  }; //
 
   using AppArray = Array<ArrayEntry, kNumArrayEntries>;
 
-  std::unique_ptr<std::mt19937> generators[helpers::kNumCPUs];
+  std::unique_ptr<std::mt19937> generators[helpers::kNumCPUs]; // 随机数数组对象
   alignas(helpers::kHugepageSize) Req all_gen_reqs[kNumReqs];
   uint32_t all_zipf_req_indices[helpers::kNumCPUs][kReqSeqLen];
 
@@ -104,17 +105,26 @@ private:
   Cnt lats_idx[helpers::kNumCPUs];
   Cnt per_core_req_idx[helpers::kNumCPUs];
 
-  std::atomic_flag flag;
+  std::atomic_flag flag; // <atomic> 提供的布尔原子类型。
   uint64_t print_times = 0;
   uint64_t prev_sum_reqs = 0;
   uint64_t prev_us = 0;
   std::vector<double> mops_records;
 
+  // CryptoPP::AES::XX 是来自 Crypto++ 库的常量。它定义了 AES 算法的默认参数。
   unsigned char key[CryptoPP::AES::DEFAULT_KEYLENGTH];
   unsigned char iv[CryptoPP::AES::BLOCKSIZE];
+  // 这个智能指针将用于存储一个CBC 模式加密器的实例。
   std::unique_ptr<CryptoPP::CBC_Mode_ExternalCipher::Encryption> cbcEncryption;
+  // 这个智能指针将用于存储一个AES 加密器的实例。
   std::unique_ptr<CryptoPP::AES::Encryption> aesEncryption;
+  // 通过使用 std::unique_ptr，可以确保这些复杂的对象在不再需要时能够被正确地释放，避免资源泄漏。
 
+  /*
+  *  inline 尝试将函数体在调用处展开，而不是进行常规的函数调用。
+  *  将一个 uint32_t 类型的非负整数 n 转换为其十进制字符串表示，并将其写入到提供的 char 数组 array 中。
+  *  转换后的字符串将具有固定的 suffix_len 长度，如果 n 的十进制位数不足 suffix_len，则会在前面填充足够的零。
+  */
   inline void append_uint32_to_char_array(uint32_t n, uint32_t suffix_len,
                                           char *array) {
     uint32_t len = 0;
@@ -129,23 +139,26 @@ private:
     std::reverse(array, array + suffix_len);
   }
 
+  // 生成长度 len 的随机字符串，并将其填充到传入的字符数组 data 中。
   inline void random_string(char *data, uint32_t len) {
-    BUG_ON(len <= 0);
-    preempt_disable();
-    auto guard = helpers::finally([&]() { preempt_enable(); });
-    auto &generator = *generators[get_core_num()];
-    std::uniform_int_distribution<int> distribution('a', 'z' + 1);
+    BUG_ON(len <= 0); // 确保 len 是一个正数。
+    preempt_disable(); // 禁用抢占，确保原子操作
+    auto guard = helpers::finally([&]() { preempt_enable(); }); //确保在函数退出时 preempt_enable() 都会被调用
+    auto &generator = *generators[get_core_num()]; // 为每个核心/线程维护一个独立的随机数生成器
+    std::uniform_int_distribution<int> distribution('a', 'z' + 1); // 定义了一个均匀分布的整数随机数范围
     for (uint32_t i = 0; i < len; i++) {
       data[i] = char(distribution(generator));
     }
   }
 
+  // 生成一个固定长度的“请求”字符串，其中包含一个随机部分和一个格式化后的线程ID (Thread ID) 部分。
   inline void random_req(char *data, uint32_t tid) {
     auto tid_len = helpers::static_log(10, kNumMutatorThreads);
     random_string(data, kReqLen - tid_len);
     append_uint32_to_char_array(tid, tid_len, data + kReqLen - tid_len);
   }
 
+  // 生成 0 到 u32 最大值的数字，并返回
   inline uint32_t random_uint32() {
     preempt_disable();
     auto guard = helpers::finally([&]() { preempt_enable(); });
@@ -155,12 +168,13 @@ private:
     return distribution(generator);
   }
 
+  // 初始化一个并发哈希表（Hopscotch），并为它准备好一个模拟真实世界工作负载的测试场景。aifm/exp/inc/concurrent_hopscotch.hpp
   void prepare(GenericConcurrentHopscotch *hopscotch) {
     for (uint32_t i = 0; i < helpers::kNumCPUs; i++) {
       std::random_device rd;
       generators[i].reset(new std::mt19937(rd()));
     }
-    memset(lats_idx, 0, sizeof(lats_idx));
+    memset(lats_idx, 0, sizeof(lats_idx)); // 清空数组 lats_ids
     memset(key, 0x00, CryptoPP::AES::DEFAULT_KEYLENGTH);
     memset(iv, 0x00, CryptoPP::AES::BLOCKSIZE);
     aesEncryption.reset(
